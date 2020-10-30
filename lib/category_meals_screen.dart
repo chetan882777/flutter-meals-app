9e8861b09ac0bdab741a26c040fa4c76ef46ac00
chetan pawar <chetan882777@gmail.com>
2020-10-30 14:11:38 +0530

    use route for navigation

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String id;
-  final String title;
-
-  CategoryMealsScreen(this.id, this.title);
+  // final String id;
+  // final String title;
+  //
+  // CategoryMealsScreen(this.id, this.title);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(categoryTitle),
       ),
       body: Container(
         child: Center(
